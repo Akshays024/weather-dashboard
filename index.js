2f5551c3ab776e0d15bcccd6399cbc78134ca1db
Akshay S <akshays7353@example.com>
2026-08-10 10:01:04 +0530

    Add weather dashboard

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const API_KEY = "76b51dc015315e87195d85cc5369d01b"
 const search = document.getElementById("search");
 const searchBtn = document.getElementById("searchBtn");
 // const container = document.getElementsByClassName("container")
